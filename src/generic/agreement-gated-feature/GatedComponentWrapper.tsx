afafc30e836bcbee6b3f0fc250d043643be20220
kshitij.sobti <kshitij@sobti.in>
2026-02-24 18:47:23 +0530

    fixup! feat: add agreement-gated feature support across files and videos pages

--- src/generic/agreement-gated-feature/GatedComponentWrapper.tsx
+++ src/generic/agreement-gated-feature/GatedComponentWrapper.tsx
@@ -1,28 +1,30 @@
-import { AgreementGated } from '@src/constants';
+import { AgreementGated } from "@src/constants";
 import {
-  getGatingAgreementTypes,
-  useUserAgreementRecords,
-} from '@src/data/apiHooks';
+    getGatingAgreementTypes,
+    useUserAgreementRecords,
+} from "@src/data/apiHooks";
 
 interface GatedComponentWrapperProps {
-  gatingTypes: AgreementGated[];
-  children: React.ReactNode;
+    gatingTypes: AgreementGated[];
+    children: React.ReactElement;
 }
 
-export const GatedComponentWrapper = ({ gatingTypes, children }: GatedComponentWrapperProps) => {
-  const agreementTypes = getGatingAgreementTypes(gatingTypes);
-  const results = useUserAgreementRecords(agreementTypes);
-  const isNotGated = results.every(result => !!result?.data?.isCurrent);
-  return isNotGated ? children : (
-    <div
-      aria-disabled
-      style={{
-        pointerEvents: 'none',
-        userSelect: 'none',
-        filter: 'blur(2px)',
-      }}
-    >
-      {children}
-    </div>
-  );
+export const GatedComponentWrapper = (
+    { gatingTypes, children }: GatedComponentWrapperProps,
+) => {
+    const agreementTypes = getGatingAgreementTypes(gatingTypes);
+    const results = useUserAgreementRecords(agreementTypes);
+    const isNotGated = results.every((result) => !!result?.data?.isCurrent);
+    return isNotGated ? children : (
+        <div
+            aria-disabled
+            style={{
+                pointerEvents: "none",
+                userSelect: "none",
+                filter: "blur(2px)",
+            }}
+        >
+            {children}
+        </div>
+    );
 };
